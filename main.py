b5916cab11df89388f100abedf19eb7e3f97023a
neplooho <workkondakov@gmail.com>
2021-01-28 13:49:58 +0200

    This should be it

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def build_telegraph_and_return_link(conn, chat_id, *args):
     if not is_ready_to_finish(session):
         missing_values = get_missing_values(session)
         send_message(chat_id, 'Зполните пожалуйсте следующие поля:\n' + ', '.join(missing_values),
-                     remove_markup=remove_markup)
+                     reply_markup=remove_markup)
         return
     links_to_download = [FILE_URL + x for x in session['images']]
     image_binaries = [requests.get(x).content for x in links_to_download]
@@ -341,7 +341,7 @@ def main():
             options[session['step']][1](conn, chat_id, data['message']['text'])
         conn.commit()
         return Response('Duck says meow')
-    except sqlite3.OperationalError as e:
+    except Exception as e:
         f.write(str(e) + '\n')
         send_message(chat_id, "Ты что-то не то отправил, попробуй ещё раз или вызови /help")
         return Response("Quack")
