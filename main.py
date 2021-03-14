7a1bf126955fa8b62ff1ab004f7bca9c0c005aa6
neplooho <workkondakov@gmail.com>
2021-03-14 22:55:13 +0200

    more debug

--- main.py
+++ main.py
@@ -401,8 +401,8 @@ def main():
         conn.commit()
         return Response('Duck says meow')
     except Exception as e:
-        f.write(str(e) + '\n')
-        # print(e.with_traceback())
+        # f.write(str(e) + '\n')
+        print(e.with_traceback())
         send_message(chat_id, "Ты что-то не то отправил, попробуй ещё раз или вызови /help")
         return Response("Quack")
     finally:
